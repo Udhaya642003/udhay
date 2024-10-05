550b245d44e654356105b7fbeda265062ad8b1ed
Udhaya kumar <udhayakumar.d@idp.com>
2024-10-05 20:57:22 +0530

    no s

--- src/app/components/pagination.tsx
+++ src/app/components/pagination.tsx
@@ -1,49 +1,51 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 interface PaginationProps {
   currentPage: number;
 }
 const Pagination: React.FC<PaginationProps> = ({ currentPage }) => {
   const totalPages = 20;
-
+  const router = useRouter();
+  const movePage = (number: any) => {
+    router.push(`/new/advice?id=${number}`);
+  };
   return (
     <div className="flex justify-center mt-4">
       <nav className="inline-flex">
-        <Link
-          prefetch={false}
-          href={
-            currentPage > 1
-              ? `/new/advice?id=${currentPage - 1}`
-              : `/new/advice?id=${currentPage}`
-          }
+        <button
+          // href={
+          //   currentPage > 1
+          //     ? `/new/advice?id=${currentPage - 1}`
+          //     : `/new/advice?id=${currentPage}`
+          // }
           className="px-3 py-1 text-sm font-medium text-gray-700 bg-gray-200 border border-gray-300 rounded-l hover:bg-gray-300"
         >
           Previous
-        </Link>
+        </button>
         {Array.from({ length: totalPages }, (_, index) => (
-          <Link
-            prefetch={false}
-            href={`/new/advice?id=${index + 1}`}
+          <button
+            onClick={() => movePage(index + 1)}
+            // href={`/new/advice?id=${index + 1}`}
             key={index + 1}
             className={`px-3 py-1 text-sm font-medium text-gray-700 bg-gray-200 border border-gray-300 hover:bg-gray-300 ${
               currentPage === index + 1 ? "bg-blue-500 text-white" : ""
             }`}
           >
             {index + 1}
-          </Link>
+          </button>
         ))}
-        <Link
-          prefetch={false}
-          href={
-            currentPage < totalPages
-              ? `?id=${currentPage + 1}`
-              : `?id=${currentPage}`
-          }
+        <button
+          // href={
+          //   currentPage < totalPages
+          //     ? `?id=${currentPage + 1}`
+          //     : `?id=${currentPage}`
+          // }
           className="px-3 py-1 text-sm font-medium text-gray-700 bg-gray-200 border border-gray-300 rounded-r hover:bg-gray-300"
         >
           Next
-        </Link>
+        </button>
       </nav>
     </div>
   );
