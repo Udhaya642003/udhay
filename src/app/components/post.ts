82d50043c11f26ef79791212ce0cda8c8d90eed3
Udhaya kumar <udhayakumar.d@idp.com>
2024-10-05 20:47:14 +0530

    no store and delay 0

--- src/app/components/post.ts
+++ src/app/components/post.ts
@@ -1,9 +1,6 @@
 "use server";
 
 const fetchPostData = async (id: any) => {
-  const delay = (ms: number) =>
-    new Promise((resolve) => setTimeout(resolve, ms));
-  await delay(1000);
   const resp = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${id}`,
     {
