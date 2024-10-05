7961eff68b7d4d47c0b93f971e0aa01d4f4474a1
Udhaya kumar <udhayakumar.d@idp.com>
2024-10-05 21:05:41 +0530

    ms config removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    staleTimes: {
-      static: 0,
-      dynamic: 0,
-    },
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
