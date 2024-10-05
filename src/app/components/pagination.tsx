da83858f30a2ac34dac3835d18434536ac2f0e1f
Udhaya kumar <udhayakumar.d@idp.com>
2024-10-05 20:23:50 +0530

    pagination added

--- src/app/components/pagination.tsx
+++ src/app/components/pagination.tsx
@@ -1,4 +1,4 @@
-"use server";
+"use client";
 import React from "react";
 import Link from "next/link";
 interface PaginationProps {
@@ -12,7 +12,9 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage }) => {
       <nav className="inline-flex">
         <Link
           href={
-            currentPage > 1 ? `?id=${currentPage - 1}` : `?id=${currentPage}`
+            currentPage > 1
+              ? `/new/advice?id=${currentPage - 1}`
+              : `/new/advice?id=${currentPage}`
           }
           className="px-3 py-1 text-sm font-medium text-gray-700 bg-gray-200 border border-gray-300 rounded-l hover:bg-gray-300"
         >
@@ -20,7 +22,7 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage }) => {
         </Link>
         {Array.from({ length: totalPages }, (_, index) => (
           <Link
-            href={`?id=${index + 1}`}
+            href={`new/advice?id=${index + 1}`}
             key={index + 1}
             className={`px-3 py-1 text-sm font-medium text-gray-700 bg-gray-200 border border-gray-300 hover:bg-gray-300 ${
               currentPage === index + 1 ? "bg-blue-500 text-white" : ""
